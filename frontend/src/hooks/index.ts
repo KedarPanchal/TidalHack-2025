974e98948ddc33fea27ae1d2416e02f9b2f9f2d2
Rohan Perumalil <rohanperumalil@gmail.com>
2025-10-25 13:13:30 -0500

    Revert "Update API to allow sending chats to different personas"
    This reverts commit 5c4653b913fe199d6612434167ace54ab752263a.

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,5 +1,3 @@
 // Export API hook
 
 export { useAPI, usePOST, useGET } from "./useapi";
-export { useChatRequest, useRAGRequest } from "./useapihelpers";
-export type { ChatPersona } from "./useapihelpers";

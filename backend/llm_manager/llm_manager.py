6ce8e59772f457ce89cb43662c7a753be2c681e2
KedarPanchal <kedar.t.panchal@outlook.com>
2025-10-25 10:20:16 -0500

    Update llm_manager test

--- backend/llm_manager/llm_manager.py
+++ backend/llm_manager/llm_manager.py
@@ -4,9 +4,8 @@ class LLMManager():
 
     def act(self, prompt: str) -> str:
         """ Generate a response based on the given prompt. """
-        return "Test string response from LLM Manager."
+        return f"Echoing prompt: {prompt}"
 
     def act_rag(self, prompt: str, context: str) -> str:
         """ Generate a response based on the given prompt and context. """
-        # Leave this blank for now we can implement later if needed
-        return "Test string response from LLM Manager with RAG."
+        return f"Echoing prompt: {prompt} with context: {context}"
